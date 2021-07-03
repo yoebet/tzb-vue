a298daec09c806f9f8907930fb356b35755f32e9
wushexu <wushexu@gmail.com>
2021-07-03 18:59:37 +0800

    el

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,11 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
+import {ElDatePicker, ElCheckbox} from 'element-plus'
+// import 'element-plus/packages/theme-chalk/src/base.scss'
 import App from './App.vue'
 import router from './router'
 
-createApp(App).use(router).mount('#app')
+createApp(App)
+  .use(router)
+  .use(ElDatePicker)
+  .use(ElCheckbox)
+  .mount('#app')
